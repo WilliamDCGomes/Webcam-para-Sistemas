2cbe3329cd597afbe1dcb02a3fb5858bb557c8e7
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2020-10-22 21:07:43 -0300

    Implementando timer de milesegundo

--- src/functioncontroller/GetHour.java
+++ src/functioncontroller/GetHour.java
@@ -7,6 +7,6 @@ public class GetHour {
         Calendar calendar = new GregorianCalendar();
         Date trialTime = new Date();
         calendar.setTime(trialTime);
-        return calendar.get(Calendar.HOUR_OF_DAY) + calendar.get(Calendar.MINUTE) + calendar.get(Calendar.SECOND);
+        return calendar.get(Calendar.HOUR_OF_DAY) + calendar.get(Calendar.MINUTE) + calendar.get(Calendar.SECOND) + calendar.get(Calendar.MILLISECOND);
     }
 }
